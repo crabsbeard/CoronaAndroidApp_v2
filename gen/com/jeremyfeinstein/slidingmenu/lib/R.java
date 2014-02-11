8653a5809eac231242207dab04fe093af72b5f62
dev-aditya <dev.aditya16@gmail.com>
2014-02-11 11:24:08 +0530

    final commit

--- gen/com/jeremyfeinstein/slidingmenu/lib/R.java
+++ gen/com/jeremyfeinstein/slidingmenu/lib/R.java
@@ -29,7 +29,7 @@ public final class R {
 		public static final int margin = 0x7f050007;
 		public static final int right = 0x7f050006;
 		public static final int selected_view = 0x7f050009;
-		public static final int slidingmenumain = 0x7f050030;
+		public static final int slidingmenumain = 0x7f05002f;
 	}
 	public static final class layout {
 		public static final int slidingmenumain = 0x7f030011;
--- src/com/cbs/corona/GoogleMaps.java
+++ src/com/cbs/corona/GoogleMaps.java
@@ -4,61 +4,44 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GoogleMaps extends Activity {
 
 	// Google Map
-	private GoogleMap googleMap;
+	public GoogleMap googleMap;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.maps_google);
-
-		try {
-			// Loading map
-			initilizeMap();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+		initilizeMap();
 	}
+
+		
 
 	/**
 	 * function to load map. If map is not created it will create it for you
 	 * */
-	private void initilizeMap() {
-		if (googleMap == null) {
-			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-					R.id.map)).getMap();
-
-			// check if map is created successfully or not
-			if (googleMap == null) {
-				Toast.makeText(getApplicationContext(),
-						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
-						.show();
-			}
-		}
-		// latitude and longitude
-		double latitude = 28.737323;
-		double longitude = 77.112152;
-
-		// create marker
-		MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Bhagwan Parshuram Institute of Technology");
-
-		// adding marker
-		googleMap.addMarker(marker);
+	public void initilizeMap() {
+		
+		googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+		        .getMap();
+		LatLng BPIT = new LatLng(28.737323,77.112152);
+		Marker hamburg = googleMap.addMarker(new MarkerOptions().position(BPIT)
+		        .title("Bhagwan Parshuram Institute of Technology"));
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+			.target(BPIT)
+			.zoom(14)
+			.build();
+		googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+		
 	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		initilizeMap();
-	}
-
 }
